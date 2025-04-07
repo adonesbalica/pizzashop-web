6c0a5b3f4e20d51148e30ca77d791fc60b2f8e90
Adones Balica <adonesbalica@gmail.com>
2025-04-07 14:31:29 -0300

    feat: initial commit

---
+++ src/rotues.tsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+import { AppLayout } from './pages/_layouts/app'
+import { AuthLayout } from './pages/_layouts/auth'
+import { Dashboard } from './pages/app/dashboard'
+import { SingIn } from './pages/auth/sign-in'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Dashboard />,
+      },
+    ],
+  },
+  {
+    path: '/sing-in',
+    element: <AuthLayout />,
+    children: [{ path: '/sing-in', element: <SingIn /> }],
+  },
+])
