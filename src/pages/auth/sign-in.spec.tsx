88601ead623100277b174445fc8bd31f411195d1
Adones Balica <adonesbalica@gmail.com>
2025-04-21 09:47:44 -0300

    feat: sing in test

---
+++ src/pages/auth/sign-in.spec.tsx
@@ -0,0 +1,25 @@
+import { QueryClientProvider } from '@tanstack/react-query'
+import { render } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+
+import { queryClient } from '@/lib/react-query'
+
+import { SignIn } from './sign-in'
+
+describe('Sign in', () => {
+  it('should set default email input value if email is present on search params', () => {
+    const wrapper = render(<SignIn />, {
+      wrapper: ({ children }) => {
+        return (
+          <MemoryRouter initialEntries={['/sign-in?email=johndoe@example.com']}>
+            <QueryClientProvider client={queryClient}>
+              {children}
+            </QueryClientProvider>
+          </MemoryRouter>
+        )
+      },
+    })
+
+    expect(wrapper.getByDisplayValue('johndoe@example.com')).toBeInTheDocument()
+  })
+})
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,21 +1,21 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useMutation } from '@tanstack/react-query'
-import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
-import { Link, useSearchParams } from 'react-router-dom'
+import { useSearchParams } from 'react-router-dom'
 import { toast } from 'sonner'
+import { twMerge } from 'tailwind-merge'
 import { z } from 'zod'
 
 import { signIn } from '@/api/sign-in'
-import { Button } from '@/components/ui/button'
+import { Button, buttonVariants } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
-const signInForm = z.object({
+const signInSchema = z.object({
   email: z.string().email(),
 })
 
-type SignInForm = z.infer<typeof signInForm>
+type SignInSchema = z.infer<typeof signInSchema>
 
 export function SignIn() {
   const [searchParams] = useSearchParams()
@@ -24,8 +24,8 @@ export function SignIn() {
     register,
     handleSubmit,
     formState: { isSubmitting },
-  } = useForm<SignInForm>({
-    resolver: zodResolver(signInForm),
+  } = useForm<SignInSchema>({
+    resolver: zodResolver(signInSchema),
     defaultValues: {
       email: searchParams.get('email') ?? '',
     },
@@ -35,53 +35,65 @@ export function SignIn() {
     mutationFn: signIn,
   })
 
-  async function handleSignIn(data: SignInForm) {
+  async function handleAuthenticate({ email }: SignInSchema) {
     try {
-      await authenticate({ email: data.email })
+      await authenticate({ email })
 
-      toast.success('Enviamos um link de autenticação para o seu e-mail.', {
+      toast.success('Enviamos um link de autenticação para seu e-mail.', {
         action: {
           label: 'Reenviar',
-          onClick: () => handleSignIn(data),
+          onClick: () => authenticate({ email }),
         },
       })
     } catch {
-      toast.error('Credenciais inválidas.')
+      toast.error('Credenciais inválidas')
     }
   }
 
   return (
-    <>
-      <Helmet title="Login" />
-      <div className="p-8">
-        <Button asChild className="absolute right-8 top-8" variant="ghost">
-          <Link to="/sing-up" className="">
-            Novo establecimento
-          </Link>
-        </Button>
+    <div className="lg:p-8">
+      <a
+        href="/sign-up"
+        className={twMerge(
+          buttonVariants({ variant: 'ghost' }),
+          'absolute right-4 top-4 md:right-8 md:top-8',
+        )}
+      >
+        Novo estabelecimento
+      </a>
 
-        <div className="flex w-[320px] flex-col justify-center gap-6">
-          <div className="flex flex-col gap-2 text-center">
-            <h1 className="text-2xl font-semibold tracking-tight">
-              Acessar painel
-            </h1>
-            <p className="text-sm text-muted-foreground">
-              Acompanhe suas vendas pelo painel do parceiro!
-            </p>
-          </div>
+      <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        <div className="flex flex-col space-y-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Acessar painel
+          </h1>
+          <p className="text-sm text-muted-foreground">
+            Acompanhe suas vendas pelo painel do parceiro!
+          </p>
+        </div>
 
-          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
-            <div className="space-y-2">
-              <Label htmlFor="email">Seu e-mail</Label>
-              <Input id="email" type="email" {...register('email')} />
+        <div className="grid gap-6">
+          <form onSubmit={handleSubmit(handleAuthenticate)}>
+            <div className="grid gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Seu e-mail</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  autoCapitalize="none"
+                  autoComplete="email"
+                  autoCorrect="off"
+                  {...register('email')}
+                />
+              </div>
+
+              <Button type="submit" disabled={isSubmitting}>
+                Acessar painel
+              </Button>
             </div>
-
-            <Button className="w-full" type="submit" disabled={isSubmitting}>
-              Aceesar painel
-            </Button>
           </form>
         </div>
       </div>
-    </>
+    </div>
   )
 }
